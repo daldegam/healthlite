0e0d0bd071c983f06e33ad7edd08ff709fe63e64
Gustavo Ferreira <Gustavo Ferreira>
2016-01-30 19:03:53 -0200

    criacao da pagina de status (apenas o layout)

---
+++ app/HealtyLite/HealtyLite/Views/Report.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace HealtyLite
+{
+	public partial class Report : ContentPage
+	{
+		public Report ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
