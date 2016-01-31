cd7a89c2fab61856fd41f3d4806d9839f60206dd
Breno Mendes <breno.hfm@gmail.com>
2016-01-30 22:46:57 -0200

    'SubindoRemedio2'

---
+++ app/HealtyLite/HealtyLite/Views/Informacoes.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace HealtyLite
+{
+	public partial class Informacoes : ContentPage
+	{
+		public Informacoes ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
--- app/HealtyLite/HealtyLite/Views/Remedio.xaml.cs
+++ app/HealtyLite/HealtyLite/Views/Remedio.xaml.cs
@@ -1,15 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Linq;
 using Xamarin.Forms;
 
-namespace HealtyLite
+namespace Xamarin4PartyBH
 {
 	public partial class Remedio : ContentPage
 	{
 		public Remedio ()
 		{
 			InitializeComponent ();
+		}
+		async void ClickedCadastrar(object sender, EventArgs e)
+		{
+
+			if (remedioNome.Text == null || frequenciaHH.Text  == null || frequenciaMM.Text  == null) {
+
+				await DisplayAlert ("ERRO", "Verifique se preencheu os campos", "Voltar");
+
+			} else {
+
+				var remedio = remedioNome.Text;
+				var frequenciaRemedioHH = frequenciaHH.Text;
+				var frequenciaRemedioMM = frequenciaMM.Text;
+
+			}
+				
 		}
 	}
 }
